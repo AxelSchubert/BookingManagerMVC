244db4f98e51c444d44bf8818223bf20fb04c830
AxelSchubert <axel.schubert@chasacademy.se>
2025-09-11 17:57:00 +0200

    Added unstylized homepage and menupage

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,20 @@
+﻿using BookingManagerMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingManagerMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Course> courses = new List<Course>
+            {
+              new Course { Id = 1, CourseName = "Pad Thai", Price = 120, Description = "Stekta risnudlar med räkor, tofu och jordnötter", IsPopular = true },
+              new Course { Id = 2, CourseName = "Grön Curry", Price = 130, Description = "Stark grön curry med kyckling och kokosmjölk", IsPopular = true },
+              new Course { Id = 3, CourseName = "Tom Yum Soppa", Price = 120, Description = "Het och sur soppa med räkor och citrongräs", IsPopular = false },
+              new Course { Id = 4, CourseName = "Stekt Ris", Price = 110, Description = "Stekt ris med kycling, biff, räkor eller tofu", IsPopular = true }
+            };
+            return View(courses);
+        }
+    }
+}
